99fc9b3cc07eff09ab10498fe31db8e6970b8d13
Tomohiro-Kawakami <63573398+Tomohiro-Kawakami@users.noreply.github.com>
2020-06-26 12:16:36 +0900

    commit

--- Flawless_ex - 0619/Flawless_ex/ClientMaster_add.cs
+++ Flawless_ex - 0619/Flawless_ex/ClientMaster_add.cs
@@ -99,11 +99,62 @@ namespace Flawless_ex
 
         private void Button18_Click(object sender, EventArgs e)
         {
+            DialogResult dr = MessageBox.Show("登録しますか？", "登録確認", MessageBoxButtons.YesNo);
+
+            if (dr == DialogResult.No)
+            {
+                return;
+            }
+
+            string RegistrationDate = this.textBox1.Text;
+            string CompanyName = this.textBox2.Text;
+            string CompanyNameKana = this.textBox3.Text;
+            int PostalCodeNumber = int.Parse(this.textBox4.Text);
+            string Address = this.textBox5.Text;
+            string AddressKana = this.textBox6.Text;
+            string ShopName = this.textBox7.Text;
+            string ShopNameKana = this.textBox8.Text;
+            string PhoneNumber = this.textBox9.Text;
+            string FaxNumber = this.textBox10.Text;
+            string Position = this.textBox11.Text;
+            string ClientStaffName = this.textBox12.Text;
+            string EmailAddress = this.textBox13.Text;
+            string BankName = this.textBox14.Text;
+            string DepositType = this.textBox15.Text;
+            string AccountName = this.textBox16.Text;
+            string URLinfor = this.textBox17.Text;
+            string BranchName = this.textBox18.Text;
+            string AccountNumber = this.textBox19.Text;
+            string AccountNameKana = this.textBox20.Text;
+            string RegisterCopy = this.textBox21.Text;
+            string Antiquelicense = this.textBox22.Text;
+            int AntiqueNumber = int.Parse(this.textBox23.Text);
+            string ID = this.textBox24.Text;
+            string PeriodStay = this.textBox46.Text;
+            string SealCertification = this.textBox26.Text;
+            string TaxCertification = this.textBox27.Text;
+            string Remarks = this.textBox28.Text;
+            string ResidenceCard = this.textBox29.Text;
+            string AolFinancialShareholder = textBox25.Text;
+            DateTime dat = DateTime.Now;
+            string b = dat.ToString("yyyy/MM/dd");
             NpgsqlConnection conn = new NpgsqlConnection();
-            //NpgsqlDataAdapter adapter;
+            NpgsqlDataAdapter adapter;
+           
+            string sql_str = "Insert into client_m_corporate VALUES (" + 0 + " , '" + RegistrationDate + "' , '" + CompanyName + "' ,'" + CompanyNameKana + "' , '" + ShopName + "' ,  '" + ShopNameKana + " ', '" + AntiqueNumber + "' , '" + PostalCodeNumber + "', '" + Address + "' , '" + AddressKana + "' , '" + PhoneNumber + "' , '" + FaxNumber + "' , '" + Position + "' , '" + ClientStaffName +
+               "' , '" + EmailAddress + "', '" + URLinfor + "', '" + BankName + "' , '" + BranchName + "' , '" + DepositType + "' , '" + AccountNumber + "' , '" + AccountName + "' , '" + AccountNameKana + "' , '" + Remarks + "' , '" + ID + "' , '" + b + "','" + Antiquelicense + "','" + TaxCertification + "','" + ResidenceCard + "','" + PeriodStay + "','" + SealCertification + "'," +
+              0 + ",'" + AolFinancialShareholder + "','" + RegisterCopy + "');";
+
             conn.ConnectionString = @"Server = 192.168.152.43; Port = 5432; User Id = postgres; Password = postgres; Database = master;"; //変更予定
+            conn.Open();
 
+            adapter = new NpgsqlDataAdapter(sql_str, conn);
+            adapter.Fill(dt);
+            MessageBox.Show("登録しました。");
+            ClientMaster clientmaster = new ClientMaster(master);
 
+            this.Close();
+            clientmaster.Show();
         }
     }
 }
