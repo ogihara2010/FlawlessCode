cb7e94c30853f100d467fb19b2e1239da244b6f5
Tomohiro-Kawakami <63573398+Tomohiro-Kawakami@users.noreply.github.com>
2020-07-29 12:38:15 +0900

    commit

--- Flawless_ex - 0619/Flawless_ex/DataSearchResults.cs
+++ Flawless_ex - 0619/Flawless_ex/DataSearchResults.cs
@@ -23,6 +23,8 @@ namespace Flawless_ex
         string staff_name;
         int staff_id;
         string data;
+        int control;
+        int antique;
         DataTable dt = new DataTable();
         public DataSearchResults(MainMenu main, int type, int id, string name1, string phoneNumber1, string address1, string item1, string search1, string search2, string search3, string data)
         {
@@ -48,55 +50,127 @@ namespace Flawless_ex
 
         private void DataSearchResults_Load(object sender, EventArgs e)
         {
-            if (type == 0)
+            #region "計算書"
+            if (data == "S")
             {
-                NpgsqlConnection conn = new NpgsqlConnection();
-                NpgsqlDataAdapter adapter;
-                conn.ConnectionString = @"Server = 192.168.152.157; Port = 5432; User Id = postgres; Password = postgres; Database = master;"; //変更予定
+                this.button1.Enabled = false;
+                #region "法人"
+                if (type == 0)
+                {
+                    NpgsqlConnection conn = new NpgsqlConnection();
+                    NpgsqlDataAdapter adapter;
+                    conn.ConnectionString = @"Server = localhost; Port = 5432; User Id = postgres; Password = postgres; Database = master;"; //変更予定
 
-                string sql_str = "select A.settlement_date, A.delivery_date, B.shop_name, B.staff_name, B.phone_number, B.address, D.item_name, C.amount from statement_data A inner join client_m_corporate B ON (A.antique_number = B.antique_number )" +
+                    string sql_str = "select A.settlement_date, A.delivery_date, B.shop_name, B.staff_name, B.phone_number, B.address, D.item_name, C.amount from statement_data A inner join client_m_corporate B ON (A.antique_number = B.antique_number )" +
+                                     "inner join statement_calc_data C ON (A.document_number = C.document_number ) inner join item_m D ON (C.main_category_code = D.main_category_code and C.item_code = D.item_code ) " +
+                                     " where B.invalid = 0 and B.shop_name = '" + name1 + "' " + search1 + " B.phone_number = '" + phoneNumber1 + "'" + " " + search2 + " B.address like '% " + address1 + " %' " + search3 + " D.item_name = '" + item1 + "';";
+                    conn.Open();
+
+                    adapter = new NpgsqlDataAdapter(sql_str, conn);
+                    adapter.Fill(dt);
+                    dataGridView1.DataSource = dt;
+                    dataGridView1.Columns[0].HeaderText = "決済日";
+                    dataGridView1.Columns[1].HeaderText = "受渡日";
+                    dataGridView1.Columns[2].HeaderText = "店舗名";
+                    dataGridView1.Columns[3].HeaderText = "担当者名・個人名";
+                    dataGridView1.Columns[4].HeaderText = "電話番号";
+                    dataGridView1.Columns[5].HeaderText = "住所";
+                    dataGridView1.Columns[6].HeaderText = "品名";
+                    dataGridView1.Columns[7].HeaderText = "金額";
+
+                    conn.Close();
+                }
+                #endregion
+                #region "個人"
+                if (type == 1)
+                {
+                    NpgsqlConnection conn = new NpgsqlConnection();
+                    NpgsqlDataAdapter adapter;
+                    conn.ConnectionString = @"Server = localhost; Port = 5432; User Id = postgres; Password = postgres; Database = master;"; //変更予定
+
+                    string sql_str = "select A.settlement_date, A.delivery_date, B.name, B.phone_number, B.address, D.item_name, C.amount from statement_data A inner join client_m_individual B ON ( A.id_number = B.id_number )" +
                                  "inner join statement_calc_data C ON (A.document_number = C.document_number ) inner join item_m D ON (C.main_category_code = D.main_category_code and C.item_code = D.item_code ) " +
-                                 " where B.invalid = 0 and B.shop_name = '" + name1 + "' " + search1 + " B.phone_number = '" + phoneNumber1 + "'" + " " + search2 + " B.address like '% " + address1 + " %' " + search3 + " D.item_name = '" + item1 + "';";
-                conn.Open();
+                                     "where B.invalid = 0 and B.name = '" + name1 + "' " + search1 + " B.phone_number = '" + phoneNumber1 + "'" + " " + search2 + " B.address like '% " + address1 + " %' " + search3 + " D.item_name = '" + item1 + "';";
+                    conn.Open();
 
-                adapter = new NpgsqlDataAdapter(sql_str, conn);
-                adapter.Fill(dt);
-                dataGridView1.DataSource = dt;
-                dataGridView1.Columns[0].HeaderText = "決済日";
-                dataGridView1.Columns[1].HeaderText = "受渡日";
-                dataGridView1.Columns[2].HeaderText = "店舗名";
-                dataGridView1.Columns[3].HeaderText = "担当者名・個人名";
-                dataGridView1.Columns[4].HeaderText = "電話番号";
-                dataGridView1.Columns[5].HeaderText = "住所";
-                dataGridView1.Columns[6].HeaderText = "品名";
-                dataGridView1.Columns[7].HeaderText = "金額";
+                    adapter = new NpgsqlDataAdapter(sql_str, conn);
+                    adapter.Fill(dt);
+                    dataGridView1.DataSource = dt;
+                    dataGridView1.Columns[0].HeaderText = "決済日";
+                    dataGridView1.Columns[1].HeaderText = "受渡日";
+                    dataGridView1.Columns[2].HeaderText = "氏名";
+                    dataGridView1.Columns[3].HeaderText = "電話番号";
+                    dataGridView1.Columns[4].HeaderText = "住所";
+                    dataGridView1.Columns[5].HeaderText = "品名";
+                    dataGridView1.Columns[6].HeaderText = "金額";
 
-                conn.Close();
+                    conn.Close();
+                }
+                #endregion
             }
-            if (type == 1)
+            #endregion
+            #region "納品書"
+            if (data == "D")
             {
-                NpgsqlConnection conn = new NpgsqlConnection();
-                NpgsqlDataAdapter adapter;
-                conn.ConnectionString = @"Server = 192.168.152.157; Port = 5432; User Id = postgres; Password = postgres; Database = master;"; //変更予定
+                this.button2.Enabled = false;
+                #region "法人"
+                if (type == 0)
+                {
+                    NpgsqlConnection conn = new NpgsqlConnection();
+                    NpgsqlDataAdapter adapter;
+                    conn.ConnectionString = @"Server = localhost; Port = 5432; User Id = postgres; Password = postgres; Database = master;"; //変更予定
 
-                string sql_str = "select A.settlement_date, A.delivery_date, B.name, B.phone_number, B.address, D.item_name, C.amount from statement_data A inner join client_m_individual B ON ( A.id_number = B.id_number )" +
-                             "inner join statement_calc C ON (A.document_number = C.document_number ) inner join item_m D ON (C.main_category_code = D.main_category_code and C.item_code = D.item_code ) " +
-                                 "where B.invalid = 0 and B.name = '" + name1 + "' " + search1 + " B.phone_number = '" + phoneNumber1 + "'" + " " + search2 + " B.address like '% " + address1 + " %' " + search3 + " D.item_name = '" + item1 + "';";
-                conn.Open();
+                    string sql_str = "select A.settlement_date, A.delivery_date, B.shop_name, B.staff_name, B.phone_number, B.address, D.item_name, C.amount, A.control_number, A.antique_number from delivery_m A inner join client_m_corporate B ON (A.antique_number = B.antique_number )" +
+                                     "inner join delivery_calc C ON (A.control_number = C.control_number ) inner join item_m D ON (C.main_category_code = D.main_category_code and C.item_code = D.item_code ) " +
+                                     " where B.invalid = 0 and B.shop_name = '" + name1 + "' " + search1 + " B.phone_number = '" + phoneNumber1 + "'" + " " + search2 + " B.address like '% " + address1 + " %' " + search3 + " D.item_name = '" + item1 + "';";
+                    conn.Open();
 
-                adapter = new NpgsqlDataAdapter(sql_str, conn);
-                adapter.Fill(dt);
-                dataGridView1.DataSource = dt;
-                dataGridView1.Columns[0].HeaderText = "決済日";
-                dataGridView1.Columns[1].HeaderText = "受渡日";
-                dataGridView1.Columns[2].HeaderText = "氏名";
-                dataGridView1.Columns[3].HeaderText = "電話番号";
-                dataGridView1.Columns[4].HeaderText = "住所";
-                dataGridView1.Columns[5].HeaderText = "品名";
-                dataGridView1.Columns[6].HeaderText = "金額";
+                    adapter = new NpgsqlDataAdapter(sql_str, conn);
+                    adapter.Fill(dt);
+                    dataGridView1.DataSource = dt;
+                    dataGridView1.Columns[0].HeaderText = "決済日";
+                    dataGridView1.Columns[1].HeaderText = "受渡日";
+                    dataGridView1.Columns[2].HeaderText = "店舗名";
+                    dataGridView1.Columns[3].HeaderText = "担当者名・個人名";
+                    dataGridView1.Columns[4].HeaderText = "電話番号";
+                    dataGridView1.Columns[5].HeaderText = "住所";
+                    dataGridView1.Columns[6].HeaderText = "品名";
+                    dataGridView1.Columns[7].HeaderText = "金額";
+                    conn.Close();
+                    DataRow row3;
+                    row3 = dt.Rows[0];
+                    antique = (int)row3["antique_number"];
+                    control = (int)row3["control_number"];
+                }
+                #endregion
+                #region "個人"
+                if (type == 1)
+                {
+                    NpgsqlConnection conn = new NpgsqlConnection();
+                    NpgsqlDataAdapter adapter;
+                    conn.ConnectionString = @"Server = localhost; Port = 5432; User Id = postgres; Password = postgres; Database = master;"; //変更予定
 
-                conn.Close();
+                    string sql_str = "select A.settlement_date, A.delivery_date, B.name, B.phone_number, B.address, D.item_name, C.amount, A.control_number, A.id_number from statement_data A inner join client_m_individual B ON ( A.id_number = B.id_number )" +
+                                 "inner join delivery_calc C ON (A.control_number = C.control_number ) inner join item_m D ON (C.main_category_code = D.main_category_code and C.item_code = D.item_code ) " +
+                                     "where B.invalid = 0 and B.name = '" + name1 + "' " + search1 + " B.phone_number = '" + phoneNumber1 + "'" + " " + search2 + " B.address like '% " + address1 + " %' " + search3 + " D.item_name = '" + item1 + "';";
+                    conn.Open();
+
+                    adapter = new NpgsqlDataAdapter(sql_str, conn);
+                    adapter.Fill(dt);
+                    dataGridView1.DataSource = dt;
+                    dataGridView1.Columns[0].HeaderText = "決済日";
+                    dataGridView1.Columns[1].HeaderText = "受渡日";
+                    dataGridView1.Columns[2].HeaderText = "氏名";
+                    dataGridView1.Columns[3].HeaderText = "電話番号";
+                    dataGridView1.Columns[4].HeaderText = "住所";
+                    dataGridView1.Columns[5].HeaderText = "品名";
+                    dataGridView1.Columns[6].HeaderText = "金額";
+                    conn.Close();
+                }
+                #endregion
             }
+            #endregion
+
 
 
         }
